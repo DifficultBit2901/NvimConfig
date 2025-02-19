879a0497e7d29dedfeed5034743eefd7ae156fbd
Difficult_Bit2901 <dbhero2901@gmail.com>
2025-02-19 21:16:23 +0100

    Added some more Plugins

---
+++ lua/custom/plugins/hop_nvim.lua
@@ -0,0 +1,33 @@
+return {
+	"smoka7/hop.nvim",
+	version = "*",
+	opts = {},
+	config = function()
+		-- place this in one of your configuration file(s)
+		local hop = require("hop")
+		hop.setup({ keys = "etovxqpdygfblzhckisuran" })
+
+		local directions = require("hop.hint").HintDirection
+		vim.keymap.set("", "f", function()
+			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+		end, { remap = true })
+		vim.keymap.set("", "F", function()
+			hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+		end, { remap = true })
+		vim.keymap.set("", "t", function()
+			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+		end, { remap = true })
+		vim.keymap.set("", "T", function()
+			hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+		end, { remap = true })
+		vim.keymap.set("", "<leader>hw", function()
+			hop.hint_words()
+		end, { desc = "[h]op [w]ord" })
+		vim.keymap.set("", "<leader>hch", function()
+			hop.hint_char1()
+		end, { desc = "[h]op [ch]ar 1" })
+		vim.keymap.set("", "<leader>hcc", function()
+			hop.hint_camel_case()
+		end, { desc = "[h]op [c]amel [c]ase" })
+	end,
+}
